cd2a15494039004156b90f3d866e87d26ad3dbd0
OANA <todirica.oanaadreea@gmail.com>
2023-03-29 23:07:45 +0300

    first commit

---
+++ src/main/java/org/example/MainMutantNeechivalent1.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class MainMutantNeechivalent1 {
+
+    /*
+    Se dau trei numere intregi a, b si c cuprinse intre 10 si 999999.
+    Sa se verifice daca cmmdc dintre suma cifrelor lui a si b este mai mare decat c.
+    Sa se verifice daca numerele a si b sunt date in ordine crescatoare.
+    Programul va intoarce un string astfel:
+     - daca a, b sau c nu sunt in intervalul mentionat se va afisa un mesaj de eroare;
+     - "Cmmdc-ul sumei cifrelor primelor doua numere este/nu este mai mare decat al treilea." daca respecta sau nu prima cerinta.
+     - "Primele doua numere sunt/nu sunt in ordine crescatoare." daca respecta sau nu a doua cerinta
+     */
+
+
+    static Integer cmmdc(Integer a, Integer b) {
+        while(a != b) {
+            if(a > b) {
+                a -= b;
+            } else {
+                b -= a;
+            }
+        }
+        return a;
+    }
+
+    static Integer sumaCifrelor(Integer a) {
+        Integer sum = 0;
+
+        while(a > 0){
+            sum += a % 10;
+            a = a / 10;
+        }
+        return sum;
+
+    }
+
+    static Boolean respectaInterval(Integer a) {
+        if(a > 10 && a <= 999999) { // am modificat a >= 10
+            return true;
+        }
+        return false;
+    }
+
+    static String cerinta1(Integer a, Integer b, Integer c) {
+        if(cmmdc(sumaCifrelor(a), sumaCifrelor(b)) > c) {
+            return "Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.";
+        }
+        return "Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.";
+    }
+
+    static String cerinta2(Integer a, Integer b) {
+        if(a < b) {
+            return "Primele doua numere sunt in ordine crescatoare.";
+        }
+        return "Primele doua numere nu sunt in ordine crescatoare.";
+    }
+
+    public static String[] calculeaza(Integer a, Integer b, Integer c) {
+        String[] result;
+
+        if(respectaInterval(a) && respectaInterval(b) && respectaInterval(c)) {
+            result = new String[2];
+            result[0] = cerinta1(a, b, c);
+            result[1] = cerinta2(a, b);
+        } else {
+            result = new String[1];
+            result[0] = "Cel putin un numar din input nu este in intervalul dorit.";
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti valoarea lui a: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Introduceti valoarea lui b:");
+        int b = scanner.nextInt();
+
+        System.out.println("Introduceti valoarea lui c: ");
+        int c = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Rezultatul este:\n" + Arrays.stream(calculeaza(a, b, c)).collect(Collectors.joining("\n")));
+    }
+}
---
+++ src/test/java/MainMutantEchivalentTest.java
@@ -0,0 +1,91 @@
+import org.example.MainMutantEchivalent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainMutantEchivalentTest {
+        /*
+    Equivalence partitioning
+
+Intrare a, b, c
+
+Domeniul pt a, b, c:
+D1 = {n | n < 10}
+D2 = {n | n >= 10 && n <= 999999}
+D3 = {n | n > 999999}
+
+Iesiri:
+I1 = {["Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."]}
+I2 = {["Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."]}
+I3 = {["Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."]}
+I4 = {["Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."]}
+I5 = {"Cel putin un numar din input nu este in intervalul dorit."}
+
+Clasele:
+C11 = { (a, b, c) | a in D1 si iesirea I5} -----> a = 0, b = 100, c = 100
+C12 = { (a, b, c) | b in D1 si iesirea I5} -----> a = 100, b = 0, c = 100
+C13 = { (a, b, c) | c in D1 si iesirea I5} -----> a = 100, b = 100, c = 0
+C21 = { (a, b, c) | a, b, c in D2 si iesirea I1} -----> a = 65, b = 146, c = 10
+C22 = { (a, b, c) | a, b, c in D2 si iesirea I2} -----> a = 146, b = 65, c = 10
+C23 = { (a, b, c) | a, b, c in D2 si iesirea I3} -----> a = 65, b = 146, c = 20
+C24 = { (a, b, c) | a, b, c in D2 si iesirea I4} -----> a = 146, b = 65, c = 20
+C41 = { (a, b, c) | a in D3 si iesirea I5} -----> a = 1000000, b = 100, c = 100
+C42 = { (a, b, c) | b in D3 si iesirea I5} -----> a = 100, b = 1000000, c = 100
+C43 = { (a, b, c) | c in D3 si iesirea I5} -----> a = 100, b = 100, c = 1000000
+
+     */
+
+    @Test
+    void equivalence(){
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(0, 100, 100)[0]);
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 0, 100)[0]);
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 100, 0)[0]);
+
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(65, 146, 10));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(146, 65, 10));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(65, 146, 20));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(146, 65, 20));
+
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(1000000, 100, 100)[0]);
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 1000000, 100)[0]);
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 100, 1000000)[0]);
+    }
+
+        /*
+    Boundary value analysis
+
+    Valorile ce trebuie testate pentru a, b, c:
+    9, 10, 999999, 1000000
+
+     */
+
+    @Test
+    void boundaries() {
+        // testam pentru a, iar b si c le consideram ca fiind valori corecte din interval
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(9, 100, 100)[0]);
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(10, 100, 100));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(999999, 100, 100));
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(1000000, 100, 100)[0]);
+
+        // testam pentru b, iar a si c le consideram ca fiind valori corecte din interval
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 9, 100)[0]);
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(100, 10, 100));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(100, 999999, 100));
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 1000000, 100)[0]);
+
+        // testam pentru c, iar a si b le consideram ca fiind valori corecte din interval
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 100, 9)[0]);
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(100, 100, 10));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(100, 100, 999999));
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(100, 100, 1000000)[0]);
+    }
+
+
+    @Test
+    void causeEffectGraphing() {
+        Assertions.assertEquals("Cel putin un numar din input nu este in intervalul dorit.", MainMutantEchivalent.calculeaza(0, 100, 100)[0]);
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(65, 146, 10));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(146, 65, 10));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(65, 146, 20));
+        Assertions.assertArrayEquals(new String[]{"Cmmdc-ul sumei cifrelor primelor doua numere nu este mai mare decat al treilea.", "Primele doua numere nu sunt in ordine crescatoare."}, MainMutantEchivalent.calculeaza(146, 65, 20));
+    }
+}
